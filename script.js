6523240ca0437330052145ed817ffd1358df2a3c
Dipan Adhikari <110901003+dumbbies0078X@users.noreply.github.com>
2024-02-06 23:08:48 +0545

    Update script.js
    Signed-off-by: Dipan Adhikari <110901003+dumbbies0078X@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*!
+* Start Bootstrap - Freelancer v7.0.3 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
